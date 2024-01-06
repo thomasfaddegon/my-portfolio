9aa3a1114b8708cde091f1c1355c9dc822d90d23
Thomas Faddegon <tomfaddegon@gmail.com>
2024-01-06 10:04:45 -0500

    installed prettier, started header section

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#4B4BFA",
+        white: "#FFFFFF",
+        black: "#000000",
+      },
+    },
+  },
+  plugins: [],
+};
