67103a52e110c8efadbeccc973311e81c111a5e4
Thomas Faddegon <tomfaddegon@gmail.com>
2024-01-31 13:38:37 -0500

    added mobile nav, fix mobile issues, changed philosophy text

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Thomas Faddegon";
+export const SITE_DESCRIPTION =
+  "Welcome to my portfolio website! I am full-stack web developer and product strategist with a passion for creating beautiful, responsive, and accessible websites.";
