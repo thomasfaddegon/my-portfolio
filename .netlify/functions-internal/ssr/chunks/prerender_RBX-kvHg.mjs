59ef79629c6deed98a31f34df3446c6e48aace8a
Thomas Faddegon <tomfaddegon@gmail.com>
2024-02-04 21:02:23 -0500

    uninstalled sendgrid

--- .netlify/functions-internal/ssr/chunks/prerender_RBX-kvHg.mjs
+++
@@ -1,9 +0,0 @@
-const noop = () => {};
-export const $ = noop;
-export const _ = noop;
-export const a = noop;
-export const b = noop;
-export const c = noop;
-export const i = noop;
-export const r = noop;
-export const t = noop;
