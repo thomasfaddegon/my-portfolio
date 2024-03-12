64d8c7cf2b04e3b4cf01cf2cbeae35438a851ca0
Thomas Faddegon <tomfaddegon@gmail.com>
2024-03-12 10:45:58 -0400

    updated margins, style tweaks

--- .netlify/functions-internal/ssr/chunks/infuriating-design-flaw-strategy-games_RXlJjis2.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./infuriating-design-flaw-strategy-games_MQ3y7iTU.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .netlify/functions-internal/ssr/chunks/using-mdx_I9NAMWZd.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./using-mdx_NejunqJc.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = ["a[data-astro-cid-eimmu3lg]{display:inline-block;text-decoration:none}a[data-astro-cid-eimmu3lg]:hover{color:#d8d8d8;cursor:pointer}a[data-astro-cid-eimmu3lg].active{font-weight:bolder;background-color:#4b4bfa}a[data-astro-cid-eimmu3lg].active:hover{background-color:#4b4bfa}\n"];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
